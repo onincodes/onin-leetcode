ef5edc684e83f965192036733e62b7dc7a6c5871
Onin Sibua <sibuanino@gmail.com>
2025-06-13 13:24:40 +0800

    feat: solve Longest Common Prefix (LeetCode #14)

--- 014_longest_common_prefix.js
+++ 014_longest_common_prefix.js
@@ -1,9 +1,4 @@
 // 14. Longest Common Prefix
-// Solved
-// Easy
-// Topics
-// premium lock icon
-// Companies
 // Write a function to find the longest common prefix string amongst an array of strings.
 
 // If there is no common prefix, return an empty string "".
